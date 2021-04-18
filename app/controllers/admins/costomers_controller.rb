ae61c80ac4cb1e0e37626a1654f0ab6255285dd1
monqcq <takuyaaaan23@gmail.com>
2021-04-18 14:54:35 +0000

    [fix]ジャンル一覧・編集画面レイアウト変更

---
+++ app/controllers/admins/costomers_controller.rb
@@ -0,0 +1,19 @@
+class Admins::CostomersController < ApplicationController
+  
+  def index
+    @costomers = Costomer.all
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    
+  end
+  
+end
