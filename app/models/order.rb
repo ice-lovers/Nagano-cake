37c9c10b734797338dfa778d3044e19eb8f7cb9b
m-musha <gnmknr@gmail.com>
2021-04-19 04:54:39 +0000

    orders/index

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,7 @@ class Order < ApplicationRecord
   
   belongs_to :customer
   has_many :order_details, dependent: :destroy
+  has_many :products, through: :order_details
   
   validates :customer_id, :total_price, :delivery_charge, :pay_type, :name, :address, :postal_code, :order_status, presence: true
   validates :total_price, :delivery_charge, numericality: { only_integer: true }
