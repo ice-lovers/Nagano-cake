fcd3cf729bd2db40bfe506f1371248c3282fe788
Tomomi-Ichihashi <ichihashi.1414.tomomi@gmail.com>
2021-04-19 00:56:51 +0000

    [Update]products

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -9,16 +9,23 @@ class Admins::ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     @product.save
-    redirect_to admins_product_path(@product.id)
+    redirect_to admins_product_path(@product)
   end
 
   def show
+     @product = Product.find(params[:id])
   end
 
   def edit
   end
   
   def update
+  end
+  
+  private
+  
+  def product_params
+    params.require(:product).permit(:image, :name, :detail, :category_id, :price, :product_status)
   end
   
 end
