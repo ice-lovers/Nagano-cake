f8b5e72f4f9d3cbfc79ba98f3d1d989e27a1d400
m-musha <gnmknr@gmail.com>
2021-04-19 03:28:34 +0000

    マージのため

--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -5,10 +5,20 @@ class Admins::OrdersController < ApplicationController
   end
 
   def show
-    @oreder = Order.find(params[:id])
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admins_order_path(@order)
   end
 
+  private
+  
+  def order_params
+    params.require(:order).permit(:order_status)
+  end 
+  
 end
