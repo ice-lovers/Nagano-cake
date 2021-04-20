d5605da0c457f143a117c642be9471439171a033
monqcq <takuyaaaan23@gmail.com>
2021-04-20 06:05:13 +0000

    [add]ファイル保存

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,34 +1,34 @@
 class Customers::CustomersController < ApplicationController
   before_action :set_customer
-  
+
   def show
-    
+
   end
 
   def edit
-   
+
   end
-  
+
   def update
     @customer.update(customer_params)
     redirect_to customers_path
   end
 
   def unsubscribe
-    
+
   end
-  
+
   def out
     @customer.update(is_deleted: true)
     reset_session
     redirect_to root_path
   end
-  
+
   private
   def set_customer
     @customer = current_customer
   end
-  
+
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
