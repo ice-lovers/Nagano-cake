151449b2e20970eefcc0885e06bcbd10e9e64456
m-musha <gnmknr@gmail.com>
2021-04-20 01:35:25 +0000

    [ADD]routes.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_074623) do
+ActiveRecord::Schema.define(version: 2021_04_19_075246) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -58,19 +58,21 @@ ActiveRecord::Schema.define(version: 2021_04_19_074623) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "deliveries", force: :cascade do |t|
+    t.integer "customer_id"
+    t.string "name"
+    t.string "address"
+    t.string "postal_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "order_details", force: :cascade do |t|
     t.integer "order_id"
     t.integer "product_id"
     t.integer "quantity"
     t.integer "production_status", default: 0, null: false
     t.integer "price"
-  end
-
-  create_table "deliveries", force: :cascade do |t|
-    t.integer "customer_id"
-    t.string "name"
-    t.string "address"
-    t.string "postal_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
