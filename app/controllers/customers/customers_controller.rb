9f2435b178b5ef10b4f249b1167d83f9d8044962
monqcq <takuyaaaan23@gmail.com>
2021-04-20 04:00:50 +0000

    [add]会員情報編集機能

--- app/controllers/customers/customers_controller.rb
+++ app/controllers/customers/customers_controller.rb
@@ -1,11 +1,29 @@
 class Customers::CustomersController < ApplicationController
+  before_action :set_customer
+  
   def show
-    @customer = current_customer
+    
   end
 
   def edit
+   
+  end
+  
+  def update
+    @customer.update(customer_params)
+    redirect_to customers_path
   end
 
   def quit
+    
+  end
+  
+  private
+  def set_customer
+    @customer = current_customer
+  end
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email)
   end
 end
