cb61e57eef0b56395a0d5877f286e0f714f3143d
m-musha <gnmknr@gmail.com>
2021-04-23 05:29:37 +0000

    showページ修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,4 +10,9 @@ class Order < ApplicationRecord
   enum pay_type: { "クレジットカード": 0, "銀行振込": 1 }
   enum order_status: { "入金待ち": 0, "入金確認": 1, "制作中":2, "発送準備":3, "発送済み":4 }
 
+  def total_price
+		order_details.to_a.sum { |order| order.total_price }
+  end
 end
+
+
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,11 +2,8 @@ class OrderDetail < ApplicationRecord
 
   belongs_to :customer
   belongs_to :product
+  belongs_to :cart
 
   enum order_status: { "着手不可": 0, "製作待ち": 1, "製作中":2, "製作完了":3 }
-
-  def total_price
-    price * quantity
-  end
 
 end
