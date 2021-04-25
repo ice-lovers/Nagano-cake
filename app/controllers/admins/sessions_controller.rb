aa943b5b273eb332038fd96211c8110b19162835
monqcq <takuyaaaan23@gmail.com>
2021-04-25 06:49:14 +0000

    [fix]サインアップ後修正

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -24,9 +24,5 @@ class Admins::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  
-  def after_sign_in_path_for(resource)
-    admins_orders_path
-  end
 
 end
