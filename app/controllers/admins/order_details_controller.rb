b34c4600b12cb39c24285cbb2cdf687b030490c2
m-musha <gnmknr@gmail.com>
2021-04-25 10:57:57 +0000

    時刻を東京に変更

--- app/controllers/admins/order_details_controller.rb
+++ app/controllers/admins/order_details_controller.rb
@@ -6,13 +6,10 @@ class Admins::OrderDetailsController < ApplicationController
     @order_detail = OrderDetail.find(params[:id])
     @order = @order_detail.order
     @order_detail.update(order_detail_params)
-    flash[:success] = "制作ステータスを変更しました"
     if @order_detail.production_status == "製作中"
-      @order.update(order_status: 2)
-      flash[:success] = "制作ステータスを変更しました"
+      @order.update(order_status: 2)            
     elsif @order.order_details.count == @order.order_details.where(production_status: "製作完了").count
       @order.update(order_status: 3)
-      flash[:success] = "制作ステータスを変更しました"
     end
       redirect_to admins_order_path(@order_detail.order)
   end
